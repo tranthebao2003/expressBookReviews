0cd927ee5df0701d087828f8029797093552f539
Tran The Bao <nbkbvohoc@gmail.com>
2025-01-08 04:47:03 -0400

    done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,8 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-
+const url = "http://localhost:5000";
+const axios = require('axios').default;
 
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
@@ -16,11 +17,34 @@ public_users.post("/register", (req,res) => {
   return res.status(200).json({message: `User ${username} created`});
 });
 
+
+const getAllBook = async (url) => {
+  try {
+    const outcome = await axios.get(url);
+    let allBook = await outcome["data"];
+    console.log(allBook);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getAllBook(url);
+
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
   return res.status(200).json(books);
 });
+
+const getAllBookDetail = async (url) => {
+  try {
+    const outcome = await axios.get(url + "/isbn/1");
+    let bookDetail = await outcome["data"];
+    console.log(bookDetail);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getAllBookDetail(url);
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
@@ -32,6 +56,18 @@ public_users.get('/isbn/:isbn',function (req, res) {
   }
  });
   
+
+const getAllBookDetailByAuthor = async (url) => {
+  try {
+    const outcome = await axios.get(url + "/author/Hans Christian Andersen");
+    let bookDetail = await outcome["data"];
+    console.log(bookDetail[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getAllBookDetailByAuthor(url);
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const author = req.params.author;
@@ -46,6 +82,18 @@ public_users.get('/author/:author',function (req, res) {
   }
   return res.status(200).json(tmpBooks);
 });
+
+
+const getAllBookDetailByTitle = async (url) => {
+  try {
+    const outcome = await axios.get(url + "/title/Fairy tales");
+    let bookDetail = await outcome["data"];
+    console.log(bookDetail[0]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getAllBookDetailByTitle(url);
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
